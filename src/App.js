6dbffd7f2ce73a5a21f08b3c5752496a7d24d485
velveet <velveet@pm.me>
2020-08-03 14:47:36 -0500

    Header component added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
+      <Header />
+      
       <HelloWorld name='Simeon'/>
     </div>
   );
