e4fe4467bf1eeee93edb7b6013957b277c14532c
velveet <velveet@pm.me>
2020-08-10 11:45:21 -0500

    tailwind.config.js modified (color black-t-50). tailwind.css recompiled. (menu, menuMask) HTML elements created. React hook showMenu created.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,14 +3,37 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Navigation() {
+
+  let menu, menuMask
+
+  const [showMenu, setShowMenu] = React.useState(false)
+
+  if(showMenu) {
+    menu = <div className="fixed bg-white left-0 top-0 shadow h-full z-50 w-4/5"> The Menu </div>
+    menuMask = 
+    <div 
+    className=" bg-black-t-50 fixed left-0 top-0 w-full h-full z-50"
+    onClick = {() => setShowMenu(false)}
+    ></div>
+  }
+
   return(
-    <span className="text-xl">
-      <nav>
+    <nav className="text-xl">
+      <span>
         <FontAwesomeIcon
           icon= {faBars}
+          onClick = {() => setShowMenu(!showMenu)}
         />
-      </nav>
-    </span>
+      </span>
+
+      { menuMask }
+
+      { menu }
+
+    </nav>
+
+    
+    
   )
 }
 
