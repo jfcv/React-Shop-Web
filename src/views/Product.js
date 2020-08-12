c41b574819842daeae720929ce0de109cde96f49
velveet <velveet@pm.me>
2020-08-12 14:30:25 -0500

    Loader component created. '.loader' stylesheet created & added to index.css. Product component modified for showing loader when loading the page.

--- src/views/Product.js
+++ src/views/Product.js
@@ -1,35 +1,56 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import Loader from '../components/Loader'
 
 function Product() {
 
     const {id} = useParams()
     const url = `https://5f333dd9cfaf5a001646a8b7.mockapi.io/api/v1/products/${id}`
-    const [product, setProduct] = useState(null)
+    const [product, setProduct] = useState({
+        loading: false,
+        data: null
+    })
+
     let content = null
 
     useEffect(() => {
+
+        setProduct({
+            loading: true,
+            data: null
+        })
+
         axios.get(url)
             .then(response => {
-                setProduct(response.data)
+                setProduct({
+                    loading: false,
+                    data: response.data
+                })
             })
     }, [url])
 
-    if(product) {
+    if (product.loading) {
+        content = 
+            <div>
+                <Loader />
+            </div>
+    }
+
+    if(product.data) {
         content = 
             <div>
                 <h1 className="text-2xl font-bold mb-3">
-                    {product.name}
+                    {product.data.name}
                 </h1>
                 <div>
-                    <img src={product.image} alt={product.name} />
+                    <img src={product.data.image} alt={product.data.name} />
                 </div>
                 <div className="font-bold text-xl mb-3">
-                    $ {product.price}
+                    $ {product.data.price}
                 </div>
                 <div>
-                    {product.description}
+                    {product.data.description}
                 </div>
             </div>       
     }
